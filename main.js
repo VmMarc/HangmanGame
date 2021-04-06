069676f3cec0fce3063cf91d550feee460d56d52
bilaz666 <victor.me.marc@gmail.com>
2021-04-06 19:15:58 +0200

    update

--- main.js
+++ main.js
@@ -2,44 +2,45 @@
 const fs = require('fs')
 const { HANGMAN } = require('./hangman')
 const readlineSync = require('readline-sync')
-const chalk = require('chalk')
+const chalk = require('chalk')//(a faire)
 const { randomInt } = require('crypto')
 
 
 /*lire le fichier dict.txt et le découper en tableau 
-+ les lettres de la meme taille*/
++ les lettres de la meme taille(check si fichier existe) */
 let dict = fs.readFileSync('./dict.txt', 'utf-8')
 let dictTab = dict.toUpperCase().split('\n')
-  //console.log(dictTab)
 
 /*récupérer un mot au hazard de ce tableau 
 + découpe lettre par lettre*/
 let n = randomInt(0, dictTab.length)
 let secretWord = dictTab[n].split('')
-  //console.log(secretWord)
 
 /*crée un tableau vide
 + crée une boucle qui l'incrémente 
 avec Underscore*/
 
 let guessTab = Array(secretWord.length).fill('_')
-  //console.log(guessTab)
+const MAX_CHANCES = 8
 
-  const MAX_CHANCES = 8
-
-//conteur pour affiché le pendu 
+//count de chances
+//count pour affiché le pendu 
 let chances = 1
 let hCount = 0
-while (chances !== MAX_CHANCES && guessTab.includes('_')){
+//lancement du jeu avec condition de retour
+while (chances !== MAX_CHANCES){
   console.log(`${HANGMAN[hCount]}  /!\\ ${guessTab.join(' ')} /!\\  -->${chances}/8<-- `)
 
+  //récup de la lettre avec readline(check a faire)
   let guessLetter = ''
   guessLetter = readlineSync.question('What\'s your guess? ').toUpperCase()
 
+  //boucle qui incrémente le tableau "vide" si la lettre correspond bien
   for (let i = 0; i < secretWord.length; i++){
     secretWord[i] === guessLetter ? guessTab[i] = guessLetter : ''
   }
 
+  // condition bonne/mauvaise réponse et incrémentation
   if (secretWord.includes(guessLetter)){
     console.log('\n\tGood Guess!')
   } else {
@@ -47,7 +48,7 @@ while (chances !== MAX_CHANCES && guessTab.includes('_')){
     chances++
     hCount++
   }
-
+  //condition de victoire/defaite
   if (chances === MAX_CHANCES){
     console.log(`\t-->${chances}/8<--\n\tGAME OVER !`)
     process.exit
@@ -55,3 +56,5 @@ while (chances !== MAX_CHANCES && guessTab.includes('_')){
     console.log(`\n\tYOU WIN!\n-->${secretWord.join('')} is the Bad Guy you\'r looking for ;)`)
   }
 }
+
+//il manque encore les checks
